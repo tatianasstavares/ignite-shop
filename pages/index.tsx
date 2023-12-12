df0f56cd52ae13bc1a1acc1968cc33206a135c02
Tatiana Tavares <tatiana.t@tamanna.com>
2023-12-12 16:52:20 +0000

    Structuring the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,24 @@
-import { styled } from '../src/styles/index'
-
-
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-  '&:hover': {
-    filter: 'brigthness(0.8)',
-  },
-})
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+import shirt from '../src/assets/shirt.svg'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt} width={520} height={480} alt="t-shirt black"/>
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt} width={520} height={480} alt="t-shirt black"/>
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
