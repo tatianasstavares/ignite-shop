3da6c1d253bc5a0218eec53c5a56490e74c58559
Tatiana Tavares <tatiana.t@tamanna.com>
2023-12-12 12:06:10 +0000

    Created Header component to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../src/styles/global"
+import logoImg from '../src/assets/logo.svg'
+import { Container, Header } from "@/styles/pages/app"
+import Image  from "next/image"
 
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
   return (
+    <Container> 
+      <Header>
+        <Image src={logoImg} alt="logo rocketseat" />
+      </Header>
       <Component {...pageProps} />
+    </Container>
+     
   )
 }
 
