b250ed9e746e36971d7e3f3cc95bb4119d2039d6
Tatiana Tavares <tatiana.t@tamanna.com>
2023-12-13 16:16:37 +0000

    Configuring Stripe

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
-import shirt from '../src/assets/shirt.svg'
+import shirt1 from '../src/assets/shirt1.png'
+import shirt2 from '../src/assets/shirt2.png'
+import shirt3 from '../src/assets/shirt3.png'
+import shirt4 from '../src/assets/shirt4.png'
 import {useKeenSlider} from 'keen-slider/react'
 
 import 'keen-slider/keen-slider.min.css'
@@ -15,28 +18,28 @@ const [sliderRef] = useKeenSlider({
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       <Product className="keen-slider__slide">
-        <Image src={shirt} width={520} height={480} alt="t-shirt black"/>
+        <Image src={shirt1} width={520} height={480} alt="t-shirt black"/>
         <footer>
           <strong>Camiseta 1</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
       <Product className="keen-slider__slide">
-        <Image src={shirt} width={520} height={480} alt="t-shirt black"/>
+        <Image src={shirt2} width={520} height={480} alt="t-shirt black"/>
         <footer>
           <strong>Camiseta 1</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
       <Product className="keen-slider__slide">
-        <Image src={shirt} width={520} height={480} alt="t-shirt black"/>
+        <Image src={shirt3} width={520} height={480} alt="t-shirt black"/>
         <footer>
           <strong>Camiseta 1</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
       <Product className="keen-slider__slide">
-        <Image src={shirt} width={520} height={480} alt="t-shirt black"/>
+        <Image src={shirt4} width={520} height={480} alt="t-shirt black"/>
         <footer>
           <strong>Camiseta 1</strong>
           <span>R$ 79,90</span>
